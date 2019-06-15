1bda9e6120aa158927c8e190484e9733f2ed7a25
ivivanov18 <ivanov.ivan.v@gmail.com>
2019-06-15 17:45:47 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const GRID_SIZE = 35;
+const GRID = [];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>
+        React Snake With <span>Hooks</span>
+      </h1>
     </div>
   );
 }
